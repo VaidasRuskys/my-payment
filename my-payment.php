2a18fd9a9588342d309f04102f9caf6deb513aa4
VaidasRuskys <vaidas.ruskys@gmail.com>
2019-07-18 15:59:25 +0300

    admin menu added

--- my-payment.php
+++ my-payment.php
@@ -13,3 +13,25 @@
  function my_thank_you_text ( $content ) {
      return $content .= '<p>my_thank_you_text</p>';
  }
+
+add_action( 'admin_menu', 'my_payment_admin_menu' );  
+
+function my_payment_admin_menu(){    
+	$page_title = 'My payment';   
+	$menu_title = 'My payment';   
+	$capability = 'manage_options';   
+	$menu_slug  = 'my-payment-info';   
+	$function   = 'dashicons-cart';   
+	$icon_url   = 'dashicons-cart';   
+	$position   = 50;    
+	
+	add_menu_page( 
+		$page_title,
+		$menu_title,
+		$capability,
+		$menu_slug,
+		$function,
+		$icon_url,
+		$position 
+	); 
+} 
